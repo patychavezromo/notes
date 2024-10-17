5989f8cf8cef0b1e95fa17a19919c79a3d5b6929
Paty Chavez <patricia.araceli.chavez.romo@gmail.com>
2024-10-17 15:55:19 -0600

    Adding a billing API

--- infra/api.ts
+++ infra/api.ts
@@ -1,11 +1,11 @@
-import { table } from "./storage";
+import { table, secret } from "./storage";
 
 // Create the API
 export const api = new sst.aws.ApiGatewayV2("Api", {
   transform: {
     route: {
       handler: {
-        link: [table],
+        link: [table, secret],
       },
       args: {
         auth: { iam: true }
@@ -25,3 +25,6 @@ api.route("GET /notes", "packages/functions/src/list.main");
 api.route("PUT /notes/{id}", "packages/functions/src/update.main");
 
 api.route("DELETE /notes/{id}", "packages/functions/src/delete.main");
+
+api.route("POST /billing", "packages/functions/src/billing.main");
+
